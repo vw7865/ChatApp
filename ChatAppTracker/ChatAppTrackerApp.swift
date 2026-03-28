f3e9326f4baa4fe6dff5f9e074b1870a4ce394da
vw7865 <47992851+vw7865@users.noreply.github.com>
2026-03-28 07:22:49 -0600

    Initial Commit

---
+++ ChatAppTracker/ChatAppTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppTrackerApp.swift
+//  ChatAppTracker
+//
+//  Created by Admin on 2026. 03. 28..
+//
+
+import SwiftUI
+
+@main
+struct ChatAppTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatAppTracker
+//
+//  Created by Admin on 2026. 03. 28..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
